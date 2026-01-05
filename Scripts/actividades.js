a5de50ede3a3edc0ff208215c1502bd5a2831fd6
sof <sofh080612@gmail.com>
2026-01-05 17:33:06 -0400

    Adición de paginas restantes

---
+++ Scripts/actividades.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    initSidebar();
+    //Inicializar dropdowns
+    $('.ui.dropdown').dropdown();
+});
+
+
+//SIDEBAR
+function initSidebar() {
+
+    //Botones de control
+    const allToggleSelectors = '#sidebar-toggle, .sidebar-toggle-btn';
+
+    //Inicializamos y controlamos el sidebar usando la API de Semantic UI
+    $('.ui.sidebar').sidebar({
+        context: $('.pusher'),
+        transition: 'overlay'
+    });
+
+    $(allToggleSelectors).click(function() {
+        //La función 'toggle' lo abrirá si está cerrado, y lo cerrará si está abierto.
+        $('.ui.sidebar').sidebar('toggle');
+    });
+}
